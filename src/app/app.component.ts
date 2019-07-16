1bafb136481a66d69e9eccc30c37049f84acce6c
Norman Manantan <norman@flags.fm>
2019-07-16 15:24:34 +0800

    Updated the test and page tile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-interview';
+  title = 'Perx Angular Interview Test';
 }
